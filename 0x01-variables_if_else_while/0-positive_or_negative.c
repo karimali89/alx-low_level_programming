c13e172a8e5d74c924fa6435a39172b28000b84b
karimali89 <karim.ali17@yahoo.com>
2023-07-14 16:01:12 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-
+/**
+ * main - Entry point
+ *
+ * Description: Assigns a random number to the variable n and
+ *              prints the last digit of the number along with
+ *              its properties.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	printf("Last digit of %d is %d ", n, last_digit);
+
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
 	else
-		printf("%d is zero\n", n);
+		printf("and is less than 6 and not 0\n");
 
 	return (0);
 }
-
