868405e478293d07dbe43629a84a64b30e8713f8
karimali89 <karim.ali17@yahoo.com>
2023-07-19 13:47:28 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-    int num1 = 0;
-    int num2 = 1;
-    int nextNum;
-    int count;
+	int num1 = 0;
+	int num2 = 1;
+	int nextNum;
+	int count;
 
-    printf("%d, %d", num1, num2);
+	printf("%d, %d", num1, num2);
 
-    for (count = 2; count < 50; count++)
-    {
-        nextNum = num1 + num2;
-        printf(", %d", nextNum);
-        num1 = num2;
-        num2 = nextNum;
-    }
+	for (count = 2; count < 50; count++)
+	{
+		nextNum = num1 + num2;
+		printf(", %d", nextNum);
+		num1 = num2;
+		num2 = nextNum;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return 0;
+	return (0);
 }
