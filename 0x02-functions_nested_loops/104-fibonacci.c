94ea3bfe9d1a6560a53bb8da9324cf8a5f46cc4a
karimali89 <karim.ali17@yahoo.com>
2023-07-19 14:46:47 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,74 +1,23 @@
+#include "main.h"
 #include <stdio.h>
 
-#define MAX_DIGITS 1000
-
 /**
- * print_array - Prints an array of integers as a single number
- * @arr: The array of integers
- * @size: The size of the array
+ * print_fibonacci - Prints the first 98 Fibonacci numbers
  */
-void print_array(int *arr, int size)
+void print_fibonacci(void)
 {
 	int i;
+	unsigned long int prev = 1, current = 2, next;
+	char separator[] = ", ";
 
-	for (i = size - 1; i >= 0; i--)
-		printf("%d", arr[i]);
-}
+	printf("%lu, %lu", prev, current); /* Print the first two numbers */
 
-/**
- * fibonacci_sum_even - Computes the sum of even Fibonacci numbers
- *                     whose values do not exceed 4,000,000
- *
- * Return: The sum of even Fibonacci numbers
- */
-unsigned long int fibonacci_sum_even(void)
-{
-	int fib1[MAX_DIGITS] = {0};
-	int fib2[MAX_DIGITS] = {0};
-	int temp[MAX_DIGITS] = {0};
-	int i, carry, sum, even_sum;
-	unsigned long int total_sum = 0;
-
-	fib1[0] = 1;
-	fib2[0] = 2;
-
-	while (1)
+	for (i = 3; i <= 98; i++)
 	{
-		even_sum = fib2[0] % 2 == 0 ? fib2[0] : 0;
-
-		for (i = 0, carry = 0; i < MAX_DIGITS; i++)
-		{
-			sum = fib1[i] + fib2[i] + carry;
-			carry = sum / 10;
-			temp[i] = sum % 10;
-		}
-
-		if (fib2[0] >= 4000000)
-			break;
-
-		total_sum += even_sum;
-
-		for (i = 0; i < MAX_DIGITS; i++)
-		{
-			fib1[i] = fib2[i];
-			fib2[i] = temp[i];
-		}
+		next = prev + current;
+		printf("%s%lu", separator, next);
+		prev = current;
+		current = next;
 	}
-
-	return (total_sum);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
- */
-int main(void)
-{
-	unsigned long int sum;
-
-	sum = fibonacci_sum_even();
-	printf("%lu\n", sum);
-
-	return (0);
+	printf("\n");
 }
