a822bf673813440ebc88a954cf73225cf63648c1
karimali89 <karim.ali17@yahoo.com>
2023-07-19 15:02:02 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,44 @@
-#include "main.h"
 #include <stdio.h>
+/**
+ * print_fib - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * @len: the length of with the fib sequance should print to.
+ *
+ * Return: void
+ */
+void print_fib(int len)
+{
+	unsigned int target, i;
+
+	unsigned int former_l, former_r,;
+	unsigned int latter_l, latter_r;
+	unsigned int fib_l, fib_r;
+
+	f = 1;
+	l = 2;
+	target = len - 2;
+	for (i = 0 ; i < target ; i++)
+	{
+		fib = f + l;
+		printf("%d : ", i + 4);
+		printf("%.0Lf" , fib);
+		f = l;
+		l = fib;
+		if (i < target - 1)
+			printf("\n");
+	}
+	putchar('\n');
+}
+
 
 /**
- * print_fibonacci - Prints the first 98 Fibonacci numbers
+ * main - Entry point,
+ *
+ *
+ * Return: Always 0 (sucess)
  */
-void print_fibonacci(void)
+int main(void)
 {
-	int i;
-	unsigned long int prev = 1, current = 2, next;
-	char separator[] = ", ";
-
-	printf("%lu, %lu", prev, current); /* Print the first two numbers */
-
-	for (i = 3; i <= 98; i++)
-	{
-		next = prev + current;
-		printf("%s%lu", separator, next);
-		prev = current;
-		current = next;
-	}
-	printf("\n");
+	print_fib(98);
+	return (0);
 }
