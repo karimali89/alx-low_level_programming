373405c4e124d8ce930c3a22014bc5697430fa8a
karimali89 <karim.ali17@yahoo.com>
2023-07-20 14:23:29 -0700

    5-sing.c

--- 0x02-functions_nested_loops/5-sing.c
+++ 0x02-functions_nested_loops/5-sing.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 
-int print_sing(int n) {
-	if (n > 0) {
-		printf("+");
-		return 1;
-	} else if (n == 0) {
-		printf("0");
-		return 0;
-	} else {
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
+ *
+ * Return: 1 and prints + if n is greater than zero,
+ *         0 and prints 0 if n is zero,
+ *        -1 and prints - if n is less than zero.
+ */
+
+int print_sing(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+       	else if (n == 0)
+       {
+		putchar('0');
+		return (0);
+	} 
+	else
+	{
 		printf("-");
-		return -1;
+		return (-1);
+	}
+	else
 	}
 }
 
-
-int main () {
+int main (void)
+{
 	int number;
-	printf("Enter anumber:");
+
+	printf("Enter a anumber:");
 	scanf("%d", &number);
 
 	int result = print_sing(number);
-	printf("\n");
-	printf("Returned value: %d\n", result);
+	putchar("\n");
+	printf("Returned value: "%d\n", result);
 
-	return 0;
+	return (0);
 }
